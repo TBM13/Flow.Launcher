1308a0edf43a2262cf3e585a131193348ae5e283
TBM13 <mateotrueba@gmail.com>
2025-06-28 11:33:58 -0300

    Revert "Controls: Card: Workaround `MS.Internal.Xaml.Parser.GenericTypeNameParser.TypeNameParserException` when cards are not inside a `CardGroup`"
    This reverts commit e2ab53ba8eed5d576ccbc709390c51ceb89e30b8.

--- Flow.Launcher/Resources/Controls/CardGroupCardStyleSelector.cs
+++ Flow.Launcher/Resources/Controls/CardGroupCardStyleSelector.cs
@@ -14,22 +14,7 @@ public class CardGroupCardStyleSelector : StyleSelector
         var itemsControl = ItemsControl.ItemsControlFromItemContainer(container);
         var index = itemsControl.ItemContainerGenerator.IndexFromContainer(container);
 
-        Card card = item as Card;
-        card.Loaded += (_, _) =>
-        {
-            Border border = (Border)card.Template.FindName("BD", card);
-            border.Margin = new Thickness(0, 0, 0, 0);
-            border.CornerRadius = new CornerRadius(0);
-            border.Background = System.Windows.Media.Brushes.Transparent;
-            border.BorderThickness = new Thickness(0, 1, 0, 0);
-
-            if (index == 0)
-                border.BorderThickness = new Thickness(0, 0, 0, 0);
-        };
-
-        if (index == 0)
-            return FirstStyle;
-
+        if (index == 0) return FirstStyle;
         if (index == itemsControl.Items.Count - 1) return LastStyle;
         return MiddleStyle;
     }
