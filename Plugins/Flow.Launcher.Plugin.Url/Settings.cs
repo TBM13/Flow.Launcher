541374e1f8bbfe8eb3b46ee5678cdb3f0da8d1b8
TBM13 <mateotrueba@gmail.com>
2025-07-14 22:37:10 -0300

    Plugins: Remove URL, Explorer & BrowserBookmark plugins

--- Plugins/Flow.Launcher.Plugin.Url/Settings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Flow.Launcher.Plugin.Url
-{
-    public class Settings
-    {
-        public string BrowserPath { get; set; }
-
-        public bool OpenInNewBrowserWindow { get; set; } = true;
-    }
-}
