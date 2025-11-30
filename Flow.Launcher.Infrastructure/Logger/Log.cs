608a6f07e5a7b4af4ea0d10bdbc9394b88e27ee0
TBM13 <mateotrueba@gmail.com>
2025-11-29 23:29:36 -0300

    Logger: Improve performance on release builds by not including log functions

--- Flow.Launcher.Infrastructure/Logger/Log.cs
+++ Flow.Launcher.Infrastructure/Logger/Log.cs
@@ -1,14 +1,14 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Flow.Launcher.Infrastructure.Logger
 {
     public static class Log
     {
+        [Conditional("DEBUG")]
         public static void Exception(string className, string message, System.Exception exception, [CallerMemberName] string methodName = "")
         {
-#if DEBUG
             System.Diagnostics.Debug.WriteLine($"EXCEPTION: {methodName}@{className}: {message}\n\n{exception}");
-#endif
         }
 
         private static string CheckClassAndMessageAndReturnFullClassWithMethod(string className, string message,
@@ -22,29 +22,32 @@ namespace Flow.Launcher.Infrastructure.Logger
             return className;
         }
 
+        [Conditional("DEBUG")]
         public static void Error(string className, string message, [CallerMemberName] string methodName = "")
         {
             LogInternal(LOGLEVEL.ERROR, className, message, methodName);
         }
 
+        [Conditional("DEBUG")]
         private static void LogInternal(LOGLEVEL level, string className, string message, [CallerMemberName] string methodName = "")
         {
-#if DEBUG
             var classNameWithMethod = CheckClassAndMessageAndReturnFullClassWithMethod(className, message, methodName);
             System.Diagnostics.Debug.WriteLine($"[{level}] {classNameWithMethod}: {message}");
-#endif
         }
 
+        [Conditional("DEBUG")]
         public static void Debug(string className, string message, [CallerMemberName] string methodName = "")
         {
             LogInternal(LOGLEVEL.DEBUG, className, message, methodName);
         }
 
+        [Conditional("DEBUG")]
         public static void Info(string className, string message, [CallerMemberName] string methodName = "")
         {
             LogInternal(LOGLEVEL.INFO, className, message, methodName);
         }
 
+        [Conditional("DEBUG")]
         public static void Warn(string className, string message, [CallerMemberName] string methodName = "")
         {
             LogInternal(LOGLEVEL.WARN, className, message, methodName);
