9337169d5a1d9734233c018996a9e42295c2a28f
TBM13 <mateotrueba@gmail.com>
2026-07-15 23:30:52 -0300

    Plugins: Explorer: Simplify settings UI by switching to `iNKORE.UI.WPF.Modern`

--- Plugins/Flow.Launcher.Plugin.Explorer/Views/ExplorerSettings.xaml.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Views/ExplorerSettings.xaml.cs
@@ -1,45 +1,18 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using Flow.Launcher.Plugin.Explorer.ViewModels;
+﻿using Flow.Launcher.Plugin.Explorer.ViewModels;
 
 namespace Flow.Launcher.Plugin.Explorer.Views;
 
 public partial class ExplorerSettings
 {
-    private readonly List<Expander> _expanders;
-
     public ExplorerSettings(SettingsViewModel viewModel)
     {
         DataContext = viewModel;
         InitializeComponent();
         DataContext = viewModel;
-
-        _expanders =
-        [
-            GeneralSettingsExpander,
-            PreviewPanelExpander
-        ];
     }
 
     private void AllowOnlyNumericInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
     {
         e.Handled = e.Text.ToCharArray().Any(c => !char.IsDigit(c));
-    }
-
-    private void Expander_Expanded(object sender, RoutedEventArgs e)
-    {
-        if (sender is Expander expandedExpander)
-        {
-            // Ensure _expanders is not null and contains items
-            if (_expanders == null || _expanders.Count == 0) return;
-
-            foreach (var expander in _expanders)
-            {
-                if (expander != null && expander != expandedExpander && expander.IsExpanded)
-                {
-                    expander.IsExpanded = false;
-                }
-            }
-        }
     }
 }
