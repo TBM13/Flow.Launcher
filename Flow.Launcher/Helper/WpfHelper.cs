073af20364fb0afc022835c09fde22b1bead52b3
TBM13 <mateotrueba@gmail.com>
2026-08-02 18:37:21 -0300

    WPF Helper: Remove unused FindVisualChild

--- Flow.Launcher/Helper/WpfHelper.cs
+++ Flow.Launcher/Helper/WpfHelper.cs
@@ -6,25 +6,6 @@ namespace Flow.Launcher.Helper;
 
 public static class WpfHelper
 {
-    public static T? FindVisualChild<T>(DependencyObject dep) where T : DependencyObject
-    {
-        if (dep is null)
-            return null;
-
-        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(dep); i++)
-        {
-            var child = VisualTreeHelper.GetChild(dep, i);
-            if (child is T t)
-                return t;
-
-            var res = FindVisualChild<T>(child);
-            if (res is not null)
-                return res;
-        }
-
-        return null;
-    }
-
     /// <summary>
     /// Transforms pixels to Device Independent Pixels used by WPF
     /// </summary>
