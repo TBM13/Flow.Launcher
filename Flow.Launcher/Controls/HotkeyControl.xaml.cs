0bb2deab75b743bf6c184a3d6d06dae98aff2de2
TBM13 <mateotrueba@gmail.com>
2026-08-12 03:56:58 -0300

    UI: Merge HotkeyDisplay into HotkeyControl

--- Flow.Launcher/Controls/HotkeyControl.xaml.cs
+++ Flow.Launcher/Controls/HotkeyControl.xaml.cs
@@ -14,10 +14,23 @@ public partial class HotkeyControl
         new PropertyMetadata(null, OnHotkeyInfoChanged)
     );
 
+    public static readonly DependencyProperty KeysProperty = DependencyProperty.Register(
+        nameof(Keys),
+        typeof(string),
+        typeof(HotkeyControl),
+        new PropertyMetadata(string.Empty, OnKeysChanged)
+    );
+
     public HotkeyInfo? HotkeyInfo
     {
         get => (HotkeyInfo?)GetValue(HotkeyInfoProperty);
         set => SetValue(HotkeyInfoProperty, value);
+    }
+
+    public string Keys
+    {
+        get => (string)GetValue(KeysProperty);
+        set => SetValue(KeysProperty, value);
     }
 
     private readonly ObservableCollection<string> _keysToDisplay = [];
@@ -26,9 +39,16 @@ public partial class HotkeyControl
     {
         InitializeComponent();
         KeysList.ItemsSource = _keysToDisplay;
+        UpdateUI();
     }
 
     private static void OnHotkeyInfoChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is HotkeyControl control)
+            control.UpdateUI();
+    }
+
+    private static void OnKeysChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         if (d is HotkeyControl control)
             control.UpdateUI();
@@ -56,13 +76,29 @@ public partial class HotkeyControl
     private void UpdateUI()
     {
         _keysToDisplay.Clear();
-        if (HotkeyInfo is null || !HotkeyInfo.Hotkey.IsValid)
+
+        HotkeyButton.IsEnabled = HotkeyInfo is not null;
+
+        if (HotkeyInfo is not null)
+        {
+            if (!HotkeyInfo.Hotkey.IsValid)
+            {
+                _keysToDisplay.Add("None");
+                return;
+            }
+
+            foreach (string key in HotkeyInfo.Hotkey.ToString().Split('+'))
+                _keysToDisplay.Add(key);
+            return;
+        }
+
+        if (string.IsNullOrEmpty(Keys))
         {
             _keysToDisplay.Add("None");
             return;
         }
 
-        foreach (string key in HotkeyInfo.Hotkey.ToString().Split('+'))
+        foreach (string key in Keys.Split('+'))
             _keysToDisplay.Add(key);
     }
 }
